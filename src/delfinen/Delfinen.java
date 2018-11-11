7f3301ee01638b4e39b21c1e9d10b18c1c2ad4ca
cph-ms782 <cph-ms782@cphbusiness.dk>
2018-11-11 18:09:48 +0100

    noget

--- src/delfinen/Delfinen.java
+++ src/delfinen/Delfinen.java
@@ -1,5 +1,8 @@
 package delfinen;
 
+import io.IOtest;
+import java.io.IOException;
+
 /**
  *
  * @author Martin Wulff
@@ -10,7 +13,19 @@ public class Delfinen {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Delfinen().go();
     }
-    
+
+    public void go() {
+        IOtest io = new IOtest();
+        for (String string : io.readFile("medlemmer.csv")) {
+            System.out.println(string);
+        }
+        
+        try {
+            io.replaceSelected("medlemmer.csv", "Lars Emil", "Emil Lars");
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
 }
