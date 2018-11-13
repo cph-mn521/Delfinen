0b7ab868ab57ea669bb4d39b45d1713f9c75e1bf
NielsB <niller-niller@hotmail.com>
2018-11-13 10:26:44 +0100

    main

---
+++ src/delfinen/Main.java
@@ -0,0 +1,15 @@
+package delfinen;
+
+import delfinen.presentation.DelfinGUI;
+/**
+ *
+ * @author Martin Wulff
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new DelfinGUI().setVisible(true);
+    }
+}
