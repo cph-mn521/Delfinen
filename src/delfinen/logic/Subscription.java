4a6336fe4c0376f08c5442bd1e2a4216ec596357
cph-ms782 <cph-ms782@cphbusiness.dk>
2018-11-14 10:19:46 +0100

    subscription

--- src/delfinen/logic/Subscription.java
+++ src/delfinen/logic/Subscription.java
@@ -1,5 +1,7 @@
 package delfinen.logic;
 
+import delfinen.logic.Member.Status;
+
 /**
  *
  * @author martin b.
@@ -8,9 +10,35 @@ public class Subscription {
     private int year;
     private Price price;
     private int age;
-    
-    
-    
 
-    
+    public Subscription(int year, Price price, int age, Status status) {
+        this.year = year;
+        this.price = price;
+        this.age = age;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Price getPrice() {
+        return price;
+    }
+
+    public void setPrice(Price price) {
+        this.price = price;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
