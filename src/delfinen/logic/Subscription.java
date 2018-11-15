c9ad1de9d7a71699624dadf0b43d58c154acf8c9
cph-mn521 <43202726+cph-mn521@users.noreply.github.com>
2018-11-15 13:30:43 +0100

    Accountant 1.0

--- src/delfinen/logic/Subscription.java
+++ src/delfinen/logic/Subscription.java
@@ -2,33 +2,33 @@ package delfinen.logic;
 
 /**
  *
- * @author martin b.
+ * @author martin b. & Martin Wulff
  */
 public class Subscription {
     private int year;
     private Member holder;
     private final float price;
 
+    
+    /**
+     * Constructor for a subscription. Contains the year the subscription was payed for.
+     * the holder of the subscription and from this calculates what price the holder had to pay.
+     * @param year      Integer. The year this subscription is valid for.
+     * @param holder    Member. The holder of this subscription.
+     */         
     public Subscription(int year, Member holder) {
         this.year = year;
         this.holder = holder;
         this.price = calcPrice();
     }
     
-    
-    public int getYear() {
-        return this.year;
-    }
-
-    public float getPrice() {
-        return this.price;
-    }
-
-    public Member getHolder() {
-        return holder;
-    }
-    
+    /**
+     * Private function for calculating price according to provided price logic.
+     * @return  Float. the actual price a member of this age has to pay.
+     */
     private float calcPrice(){
+        if(holder.getStatus() == Member.Status.Passive) return 500;
+        
         int age = this.holder.getAge();
         if (age < 18) {
             return 1000;
@@ -38,5 +38,36 @@ public class Subscription {
         }
         else return (float) (1600 * 0.75);
         }
+    
+    
+    
+    
+    
+    
+    ///////////////////////      GETTERS AND SETTERS       ////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////    
+    /**
+     * Getter for this.Year.
+     * @return 
+     */
+    public int getYear() {
+        return this.year;
+    }
+    /**
+     * Getter for this.price.
+     * @return 
+     */
+    public float getPrice() {
+        return this.price;
+    }
+    
+    /**
+     * Getter for this.holder.
+     * @return 
+     */
+    public Member getHolder() {
+        return holder;
+    }
+    
         
 }
