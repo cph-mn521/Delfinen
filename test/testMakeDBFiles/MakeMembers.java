1ad04b8363c71019243469739d3b0d836af1155d
cph-ms782 <cph-ms782@cphbusiness.dk>
2018-11-20 09:17:04 +0100

    make_DB_files_added

---
+++ test/testMakeDBFiles/MakeMembers.java
@@ -0,0 +1,11 @@
+package testMakeDBFiles;
+
+/**
+ * Automatic making of members, as it can be rather cumbersome to do it manually
+ * @author martin b.
+ */
+public class MakeMembers {
+    
+    
+    
+}
